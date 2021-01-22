96611d8efaa512ca9827d3dbdb7fdf12e9149442
jpassgo <jeffpascoe9@gmail.com>
2021-01-22 16:26:41 -0600

    Updating App.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { Dispatch } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useSelector, shallowEqual, useDispatch } from 'react-redux';
+import { AddSkill } from './components/AddSkill';
+import { Skill } from './components/Skill';
 
-function App() {
+
+const App: React.FC = () => {
+  const skills: readonly Skill[] = useSelector(
+      (state: SkillState) => state.skills,
+      shallowEqual
+  )
+  const dispatch: Dispatch<any> = useDispatch()
+
+  const saveSkill = React.useCallback(
+      (skill: Skill) => dispatch(AddSkill(skill)),
+      [dispatch]
+  )
+
   return (
-    
-  );
+      <main>
+          <h1>My Skills</h1>
+          <AddSkill saveSkill={saveSkill} />
+          {skills.map((skill: Skill) => (
+              <Skill
+                  key={skill.id}
+                  skill={skill}
+                  removeSkill={removeSkill}
+              />
+          ))}
+      </main>
+  )
 }
+
+export default App
 
 export default App;
